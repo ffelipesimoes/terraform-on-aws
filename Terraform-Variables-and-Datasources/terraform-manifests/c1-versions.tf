53951ca4e259ef1b7394838ff5d09a6c216dd634
Felipe Simões <ffelipesimoes@gmail.com>
2022-07-26 06:40:04 -0300

    Create EC2 Instance using Variables and datasources

---
+++ Terraform-Variables-and-Datasources/terraform-manifests/c1-versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = "~> 1.00"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
+
